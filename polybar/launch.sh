7802f0c0c385a9ea8eca890f46d886ea0eb2b8ff
Ghost-hk <ibradawdi1@gmail.com>
2023-07-28 14:32:19 +0300

    added more cool shit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 
 
-my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-A-0')
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
 if [[ $my_laptop_external_monitor = *connected* ]]; then
     polybar secondary &
 fi
